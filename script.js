428c5d53f016a63411119408a0ed190c3ef4ced4
ronaldlehot <ronaldlehot@gmail.com>
2022-11-17 20:38:07 +0800

    revisi4

--- script.js
+++ script.js
@@ -31,3 +31,26 @@ function switchTema() {
   const tema = document.getElementById("tema").value;
   document.getElementsByTagName("meta")[2].content = tema;
 }
+//follow cursor on mousemove
+const cursor = document.querySelector(".cursor");
+var timeout;
+document.addEventListener("mousemove", (e) => {
+  let x = e.pageX;
+  let y = e.pageY;
+
+  cursor.style.top = y + "px";
+  cursor.style.left = x + "px";
+  cursor.style.display = "block";
+
+  //cursor effects when mouse stopped
+  function mouseStopped() {
+    cursor.style.display = "none";
+  }
+  clearTimeout(timeout);
+  timeout = setTimeout(mouseStopped, 10000);
+});
+
+//cursor effects when mouseout
+document.addEventListener("mouseout", () => {
+  cursor.style.display = "none";
+});
